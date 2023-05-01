3a0cdc059f5e16c0fc41ab770db0cb499b441dcb
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-05-01 09:46:41 -0400

    Added orders skeleton

---
+++ public/src/components/orders/dummyData.js
@@ -0,0 +1,40 @@
+var Data = [
+  {
+    name: 'Snake Eater: Survival Cooking 101',
+    author: 'Lali Lulelo',
+    status: 'loaned',
+    pending: false
+  },
+  {
+    name: 'The Fancy Life of Sqwilliam Fancyson',
+    author: 'Sqwilliam Fancyson',
+    status: 'borrowed',
+    pending: true
+  },
+  {
+    name: 'Raw Potatoes: The New Superfood',
+    author: 'Papas Crudas',
+    status: 'borrowed',
+    pending: false
+  },
+  {
+    name: 'The Krabby Patty Secret Formula',
+    author: 'E. Krabs',
+    status: 'borrowed',
+    pending: false
+  },
+  {
+    name: 'How to Build a Basement Eel Pond',
+    author: 'C. Fgkmm',
+    status: 'loaned',
+    pending: false
+  },
+  {
+    name: 'Life of 3.1415926535897932384626433832795029',
+    author: 'Al Gebra',
+    status: 'loaned',
+    pending: true
+  }
+]
+
+export default Data;
