37da8e93cf055776bfba1a6ee1b9a6758e22eecd
fig <arielfishkin@gmail.com>
2023-05-02 08:33:07 -0700

    added header component

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import Header from './components/homepage/Header.js';
 
 const App = () => {
+
 //conditional rendering of Gallery or Detail
+return (
+
+  <div>
+    <Header />
+  </div>
+
+)
 }
 
 export default App;
---
+++ public/src/components/homepage/Header.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+const Header = () => {
+  //conditional rendering of Gallery or Detail
+
+  return (
+    <div>
+
+      <div class="container-md border">Hello World!</div>
+
+    </div>
+
+  )
+  }
+
+  export default Header;
