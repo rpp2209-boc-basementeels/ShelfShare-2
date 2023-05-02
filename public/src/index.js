b7387baacfb69da050812cd0b1e095c9b8612d27
fig <arielfishkin@gmail.com>
2023-05-01 20:20:01 -0700

    added style loader dependencies, stylesheet rel in index.html, import bootstrap modules into index.js

--- public/src/index.js
+++ public/src/index.js
@@ -3,6 +3,10 @@ import AdditionalInformation from './components/authorization/additionalInformat
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 // import css from './Components/home.css';
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Bootstrap Bundle JS
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const node = document.getElementById('app');
 const root = createRoot(node);
