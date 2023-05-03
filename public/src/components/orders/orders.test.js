5e928ba8a176bb48eef57bec3bf889c65faff093
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-05-03 10:31:48 -0400

    Add jest testing, react testing, and jsdom tests

---
+++ public/src/components/orders/orders.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen, getByText } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Orders from './orders.jsx';
+
+
+describe("Example tests", function () {
+  test('sample test', () => {
+    expect(true).toEqual(true);
+  });
+});
+
+describe("Renders Orders", function () {
+
+  test("Orders is a function", () => {
+    expect(typeof Orders).toBe('function')
+  });
+
+  test('renders here', () => {
+    render(<Orders/>);
+    expect(screen.getByText(/here/)).toBeInTheDocument();
+  });
+});
