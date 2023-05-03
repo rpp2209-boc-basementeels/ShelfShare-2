cb1661daa3110e8a5e01941202928ff2bb5f9c40
fig <arielfishkin@gmail.com>
2023-05-03 07:12:44 -0700

    setup react-bootstrap and added sample card tiles on homepage

--- public/src/index.js
+++ public/src/index.js
@@ -4,8 +4,6 @@ import App from './components/App.jsx';
 // import css from './Components/home.css';
 // Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
-// Bootstrap Bundle JS
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const node = document.getElementById('app');
 const root = createRoot(node);
