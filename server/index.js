3adc7102775166f42a7c83dd5ade3ca185df61c5
fig <arielfishkin@gmail.com>
2023-05-03 19:31:56 -0700

    homepage route skeletons

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const addToLibrary = require('../database/addToLibrary.js')
+const addToLibrary = require('../database/addToLibrary.js');
 
 
 app.use(express.static(path.join(__dirname, "./public/dist")));
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/dist/index.html'))
+  res.sendFile(path.join(__dirname, '../public/dist/index.html'));
 });
 
 app.post('/books', (req, res) => {
@@ -31,9 +31,32 @@ app.post('/', (req, res) => {
 });
 
 app.get('/bundle.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/dist/bundle.js'))
+  res.sendFile(path.join(__dirname, '../public/dist/bundle.js'));
+});
+
+//HOMEPAGE ROUTES
+
+//GET all trending
+app.get('/trending', (req, res) => {
+
+});
+
+//GET all from selected genre
+app.get('/genre', (req, res) => {
+
+});
+
+//GET all from selected pub date range
+app.get('/published', (req, res) => {
+
+});
+
+//GET details for selected books
+app.get('/detail', (req, res) => {
+
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`App listening on port 3000`)
-})
+  console.log(`App listening on port 3000`);
+});
+
