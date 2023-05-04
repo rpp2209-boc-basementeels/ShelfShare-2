ee5382592a3e5f1ed27f484845f61d9dd33693ff
fig <arielfishkin@gmail.com>
2023-05-03 21:22:26 -0700

    added state to render book detail when book tile is clicked

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -6,10 +6,12 @@ import PersonalLibrary from './library/PersonalLibrary.jsx'
 import Button from 'react-bootstrap/Button';
 import ProfilePage from './profile/ProfilePage.jsx';
 import Orders from './orders/orders.jsx';
+import Detail from './book detail/Detail.jsx';
 
 const App = () => {
   const [clickedOnMyProfile, setClickedOnMyProfile] = useState(false);
   const [clickedOnOrder, setClickedOnOrder] = useState(false);
+  const [showBookDetail, updateShowBookDetail] = useState(false);
 
 //conditional rendering of Gallery or Detail
 return (
@@ -18,7 +20,7 @@ return (
     <Button variant="outline-primary" onClick={() => {setClickedOnMyProfile(!clickedOnMyProfile)}}>My Profile</Button>
     {clickedOnMyProfile ? <ProfilePage/> : null}
     <Header />
-    <Gallery />
+    {showBookDetail ? <Detail setBookClicked={updateShowBookDetail}/> : <Gallery setBookClicked={updateShowBookDetail}/>}
     {/* <Footer /> */}
     <button onClick={() => {setClickedOnOrder(!clickedOnOrder)}}>My Orders </button>
     {clickedOnOrder ? <Orders/> : null}
