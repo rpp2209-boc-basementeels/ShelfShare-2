161aa5ff6b05493f41cbb2ed9ee91f3be5bb8128
Maddie Sime <madisonfaithsime@gmail.com>
2023-05-03 19:46:51 -0700

    Deleted extra index.jsx in public/src

--- public/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-
-// Render the App component
-ReactDOM.render(<App />, document.getElementById('app'));
