c9d768f07aa66f104cc2c972982c556d953608a4
fig <arielfishkin@gmail.com>
2023-05-03 19:20:07 -0700

    commented out DB file

---
+++ public/src/components/homepage/homepage.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen, getByText } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Header from './Header.jsx';
+
+
+describe("Example tests", function () {
+  test('sample test', () => {
+    expect(true).toEqual(true);
+  });
+});
+
+describe("Renders the Header", function () {
+
+  // test("Orders is a function", () => {
+  //   expect(typeof Orders).toBe('function')
+  // });
+
+  test('renders here', () => {
+    render(<Header/>);
+    expect(screen.getByText(/Header/)).toBeInTheDocument();
+  });
+});
--- server/index.js
+++ server/index.js
@@ -18,14 +18,15 @@ app.get('/', (req, res) => {
 });
 
 app.post('/books', (req, res) => {
-  console.log('req body', req.body)
+  console.log('req body', req.body);
   // return addToLibrary(req.body)
   // .then(() => {
   //   res.status(200).send();
   // })
   // .catch((err) => {
   //   res.status(200).send('An error adding a book to the library: ', err);
-  // })
+});
+
 app.post('/', (req, res) => {
 });
 
