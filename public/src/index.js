17d59c90bb6b79fb71ae7b8fe1c6fe2742fe633e
Kevin Hoang <knhoangre@gmail.com>
2023-05-06 10:58:07 -0400

    Completed the functionality (front and back end) of the authorization.

--- public/src/index.js
+++ public/src/index.js
@@ -2,7 +2,10 @@ import { createRoot } from 'react-dom/client';
 import React from 'react';
 import App from './components/App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import GoogleSignIn from './components/authorization/googleSignIn.jsx';
+import AdditionalInformation from './components/authorization/additionalInformation.jsx';
+
 
 const node = document.getElementById('app');
 const root = createRoot(node);
-root.render(<App />);
+root.render(<GoogleSignIn />);
