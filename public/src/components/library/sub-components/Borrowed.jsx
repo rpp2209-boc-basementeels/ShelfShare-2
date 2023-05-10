5dea3f674eaae9d9453f9e903c57992b56e81a2b
mcpeck <peckmelodie@gmail.com>
2023-05-09 23:01:13 -0500

    Create initial skeleton for lent/borrowed books

--- public/src/components/library/sub-components/Borrowed.jsx
+++ public/src/components/library/sub-components/Borrowed.jsx
@@ -1,7 +1,72 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Borrowed = () => {
+const Borrowed = ({ libraryOwner }) => {
+  const [borrowed, setBorrowed] = useState({data: []});
+  const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetch() {
+      try {
+        const {data} = await axios.get(`http://localhost:8080/${libraryOwner}/borrowed`);
+        const out = data.reduce((a, v) => {
+          if(a[v.title]) {
+            a[v.title].author = [a[v.title].author, v.author].join(', ')
+          } else {
+            a[v.title] = v
+          }
+          return a
+        }, {});
+        setBorrowed({data: Object.values(out)});
+      } catch (err) {
+        console.log('error',err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetch();
+  }, [])
+
+  return (
+    <div>
+      {borrowed.data ?
+      <div className='Result-container'>
+      <div className='Result-header'>Borrowed Books ({borrowed.data.length})</div>
+        <div className='Result-section'>
+        <table className={'Qrcode-result-table'}>
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>Cover</td>
+              <td>Title</td>
+              <td>Author(s)</td>
+              <td>ISBN</td>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            borrowed.data.map((result, i) => {
+              return (
+                <tr key={i + 1}>
+                  <td>{i + 1}</td>
+                  <td><img src={result.image_url}/></td>
+                  <td>{result.title}</td>
+                  <td>
+                    {result.author}
+                  </td>
+                  <td>{result.isbn}</td>
+                </tr>
+                );
+            })
+          }
+          </tbody>
+        </table>
+        </div>
+        </div>
+      : null}
+    </div>
+  );
 };
 
 export default Borrowed;
--- public/src/components/library/sub-components/Lent.jsx
+++ public/src/components/library/sub-components/Lent.jsx
@@ -1,60 +1,72 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-// const Lent = ({ libraryOwner }) => {
-//   const [lent, setLent] = useState({data: []});
-//   const [isLoading, setIsLoading] = useState(false);
+const Lent = ({ libraryOwner }) => {
+  const [lent, setLent] = useState({data: []});
+  const [isLoading, setIsLoading] = useState(false);
 
-//   const fetchLent = async () => {
-//     setIsLoading(true);
-//     try {
-//       const {data} = await axios.get(`http://localhost:8080/${libraryOwner}/lent`);
-//       setLent({data: Object.values(data)});
-//     } catch (err) {
-//       console.log('error',err.message);
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetch() {
+      try {
+        const {data} = await axios.get(`http://localhost:8080/${libraryOwner}/lent`);
+        const out = data.reduce((a, v) => {
+          if(a[v.title]) {
+            a[v.title].author = [a[v.title].author, v.author].join(', ')
+          } else {
+            a[v.title] = v
+          }
+          return a
+        }, {});
+        setLent({data: Object.values(out)});
+      } catch (err) {
+        console.log('error',err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetch();
+  }, [])
 
-//   return (
-//     <div>
-//       {library.data ?
-//       <div className='Result-container'>
-//       <div className='Result-header'>My Shelf ({library.data.length})</div>
-//         <div className='Result-section'>
-//         <table className={'Qrcode-result-table'}>
-//           <thead>
-//             <tr>
-//               <td>#</td>
-//               <td>Cover</td>
-//               <td>Title</td>
-//               <td>Author(s)</td>
-//               <td>ISBN</td>
-//             </tr>
-//           </thead>
-//           <tbody>
-//           {
-//             library.data.map((result, i) => {
-//               return (
-//                 <tr key={i + 1}>
-//                   <td>{i + 1}</td>
-//                   <td><img src={result.image_url}/></td>
-//                   <td>{result.title}</td>
-//                   <td>
-//                     {result.author}
-//                   </td>
-//                   <td>{result.isbn}</td>
-//                 </tr>
-//                 );
-//             })
-//           }
-//           </tbody>
-//         </table>
-//         </div>
-//         </div>
-//       : null}
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      {lent.data ?
+      <div className='Result-container'>
+      <div className='Result-header'>Lent Books ({lent.data.length})</div>
+        <div className='Result-section'>
+        <table className={'Qrcode-result-table'}>
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>Cover</td>
+              <td>Title</td>
+              <td>Author(s)</td>
+              <td>ISBN</td>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            lent.data.map((result, i) => {
+              return (
+                <tr key={i + 1}>
+                  <td>{i + 1}</td>
+                  <td><img src={result.image_url}/></td>
+                  <td>{result.title}</td>
+                  <td>
+                    {result.author}
+                  </td>
+                  <td>{result.isbn}</td>
+                </tr>
+                );
+            })
+          }
+          </tbody>
+        </table>
+        </div>
+        </div>
+      : null}
+    </div>
+  );
+};
 
-// export default Lent;
+export default Lent;
