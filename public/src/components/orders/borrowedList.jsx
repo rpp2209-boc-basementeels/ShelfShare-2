a43effac9e94f1f013a1947b7d7095e7594441ea
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-05-11 22:47:15 -0400

    add initial error boundaries

--- public/src/components/orders/borrowedList.jsx
+++ public/src/components/orders/borrowedList.jsx
@@ -6,29 +6,39 @@ import Button from 'react-bootstrap/Button';
 import { Accordion } from "react-bootstrap";
 
 var BorrowedList = (props) => {
-let books = props.borrow;
+  let books = props.borrow;
 
-useEffect(() => {
-  BorrowedList;
-}, [props])
+  useEffect(() => {
+    BorrowedList;
+  }, [props])
 
   var index = 0;
-  return (
-    <div>
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Return Date</th>
-        </tr>
-      </thead>
-      {books.map(book => (
-        <BorrowedSingle data={book} key={index += 1} />
-      ))}
-    </table>
-  </div>
-  )
+  let borrowedBoundary = (array) => {
+    if (array.length < 1) {
+      return (
+        <div>You currently have no books borrowed</div>
+      )
+    } else {
+      return (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Return Date</th>
+              </tr>
+            </thead>
+            {books.map(book => (
+              <BorrowedSingle data={book} key={index += 1} />
+            ))}
+          </table>
+        </div>
+      )
+    }
+  }
+
+  return borrowedBoundary(books);
 }
 
 export default BorrowedList;
--- public/src/components/orders/loanedList.jsx
+++ public/src/components/orders/loanedList.jsx
@@ -6,27 +6,38 @@ import Button from 'react-bootstrap/Button';
 var LoanedList = (props) => {
   let books = props.loan;
   var index = 0;
-  console.log('ll', props)
 
   useEffect(() => {
     LoanedList;
   }, [props])
 
-  return (
-    <div>
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Author</th>
-        </tr>
-      </thead>
-      {books.map(book => (
-        <LoanedSingle data={book} key={index += 1} />
-      ))}
-    </table>
-  </div>
-  )
+  var index = 0;
+
+  let loanedBoundary = (array) => {
+    if (array.length < 1) {
+      return (
+        <div>You currently have no books loaned</div>
+      )
+    } else {
+      return (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Author</th>
+              </tr>
+            </thead>
+            {books.map(book => (
+              <LoanedSingle data={book} key={index += 1} />
+            ))}
+          </table>
+        </div>
+      )
+    }
+  }
+
+  return loanedBoundary(books);
 }
 
 
