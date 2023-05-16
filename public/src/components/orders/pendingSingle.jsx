b5a956094ec37b4a6f90ae9589f96be6b7e33dd0
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-05-15 23:03:53 -0400

    Fix order padding

--- public/src/components/orders/pendingSingle.jsx
+++ public/src/components/orders/pendingSingle.jsx
@@ -5,10 +5,10 @@ var PendingSingle = (props) => {
   return (
     <tbody>
       <tr>
-        <td>{props.data.details[0].title}</td>
-        <td>{props.data.details[0].author}</td>
-        <td>{props.data.type}</td>
-        <td><Button onClick={props.func}>Confirm</Button></td>
+        <td className="p-2">{props.data.details[0].title}</td>
+        <td className="p-2">{props.data.details[0].author}</td>
+        <td className="p-2">{props.data.type}</td>
+        <td className="p-2"><Button onClick={props.func}>Confirm</Button></td>
       </tr>
     </tbody>
   )
