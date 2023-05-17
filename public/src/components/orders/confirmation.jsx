3dde40794f5909f490d4fd0524b8cea85f16eb7d
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-05-16 22:57:58 -0400

    work on card component

--- public/src/components/orders/confirmation.jsx
+++ public/src/components/orders/confirmation.jsx
@@ -2,44 +2,31 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import test from './test.jpeg';
 
-var Confirmation = (props) => {
+var Confirmation = () => {
   return (
 
-    <div>
-      <p>Hey There!</p>
-      <p>Please ship the book to our Distribution Center at:</p>
-      <p>Hummingbird Laboratorios DC</p>
-      <p>119 Nueces Street</p>
-      <p>Austin, TX 78701</p>
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={require('./test.jpeg')}/>
+    <Card.Body>
+      <Card.Title>Shipping Confirmed!</Card.Title>
+
+      <ListGroup variant="flush">
+        <ListGroup.Item>Hey There! Please ship the book to our Distribution Center at:</ListGroup.Item>
+        <ListGroup.Item>Hummingbird Laboratorios DC</ListGroup.Item>
+        <ListGroup.Item>119 Nueces Street</ListGroup.Item>
+        <ListGroup.Item>Austin, TX 78701</ListGroup.Item>
+      </ListGroup>
+
       <Button onClick={(e) => {
         e.preventDefault();
         window.location.reload(false);
       }}>Close</Button>
-      </div>
+    </Card.Body>
+  </Card>
 
-  //   <Modal
-  //     size="lg"
-  //     aria-labelledby="contained-modal-title-vcenter"
-  //     centered
-  //   >
-  //     <Modal.Body style={{display: 'flex'}}>
-  //       {/* <img src={props.image_url} style={{ height: "10rem" }}/> */}
-  //       <ListGroup style={{paddingLeft: '0.75rem', width:'100%'}}>
-  //         <ListGroup.Item>Hey There!</ListGroup.Item>
-  //         <ListGroup.Item>Please ship the book to our Distribution Center at:</ListGroup.Item>
-  //         <ListGroup.Item>Hummingbird Laboratorios DC</ListGroup.Item>
-  //         <ListGroup.Item>119 Nueces Street</ListGroup.Item>
-  //         <ListGroup.Item>Austin, TX 78701</ListGroup.Item>
-  //       </ListGroup>
-  //     </Modal.Body>
-  //     <Modal.Footer>
-  //       <Button onClick={(e) => {
-  //         e.preventDefault();
-  //         window.location.reload(false);
-  //       }}>Close</Button>
-  //     </Modal.Footer>
-  //   </Modal>
   );
 }
 
