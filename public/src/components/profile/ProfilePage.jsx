84e16cdff6f005809f9fe0e5b62b136f2337bafd
Maddie Sime <madisonfaithsime@gmail.com>
2023-05-17 12:18:42 -0400

    Re-worked ProfilePage (personal) component to get rid of unnecessary API call; just using the user info passed down by App.jsx

--- public/src/components/profile/ProfilePage.jsx
+++ public/src/components/profile/ProfilePage.jsx
@@ -6,46 +6,22 @@ import axios from 'axios';
 const ProfilePage = (props) => {
     // props.user is the data about the current user passed down from App.jsx
     // Is not implemented by Kevin yet, so use below username as placeholder
-    var username = 'Kevin';
 
     const [userReviews, setUserReviews] = useState([]);
-    const [userInfo, setUserInfo] = useState({
-        line_1: '',
-        line_2: '',
-        city: '',
-        state: '',
-        postal: '',
-        country: '',
-        age: '',
-        email: '',
-        first_name: '',
-        last_name: '',
-        gender: '',
-        is_library: false,
-        photo_url: ''});
 
     useEffect(() => {
-        axios.get(`/reviews/${username}`)
+        axios.get(`/reviews/${props.user.username}`)
           .then((reviewData) => {
             setUserReviews(reviewData.data);
           })
           .catch((error) => {
             console.log("There was an error while trying to retrieve the user's reviews", error);
           })
-          .then(() => {
-            return axios.get(`/personalInformation/${username}`);
-          })
-          .then((infoData) => {
-            setUserInfo(infoData.data[0]);
-          })
-          .catch((error) => {
-            console.log("There was an error while trying to retrieve the user's personal information", error);
-          })
     }, []);
 
     return (
         <div>
-            <Information info={userInfo} isLibrary={userInfo.isLibrary}/>
+            <Information info={props.user} isLibrary={props.user.is_library}/>
             <div style={{"display": "flex", "alignItems": "flex-start", "justifyContent": "center"}}>
                 <div style={{"textAlign": "center", "position": "relative", "width": "30vw", "minWidth": "max-content"}}>
                     <hr></hr>
