26c1c3658d975b5789511ba919dd1644c73dcbc0
fig <arielfishkin@gmail.com>
2023-05-18 18:15:33 -0700

    gallery test

--- public/src/components/homepage/homepage.test.js
+++ public/src/components/homepage/homepage.test.js
@@ -23,10 +23,16 @@ describe("Renders the Header", function () {
   });
 });
 
-describe("Renders the Header", function () {
+describe("Renders the Gallery", function () {
 
   test('Displays `no matching titles` message when there are not books to display', () => {
     render(<Gallery books={[]}/>);
     expect(screen.getByText(/Sorry, No Matching Titles!/)).toBeInTheDocument();
   });
+
+  test('Displays `no matching titles` message when there are not books to display', () => {
+    render(<Gallery books={[{}]}/>);
+    expect(screen.getByText(/More Information/)).toBeInTheDocument();
+  });
+
 });
